6802a689c6f9d6572aaba27b18be8c9f8d2e2231
Branka Jokanovic <branka.jokanovic1@gmail.com>
2024-07-30 22:44:17 +0200

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -28,11 +28,9 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center  p-24  m-auto ">
       <div className=" pb-10 mx-auto text-center flex flex-col items-start -center max-w-3xl">
         <h1 className=" text-4xl  font-bold  text-gray-900 sm:text-6xl ">
-          Meet Your Personal <span className=" text-blue-600"> Recipe AI </span>
+          Scenario Generation  </span>
           <p className=" mt-10 font-medium   text-lg  max-w-prose text-gray-900 ">
-            Simply type few ingredients using the format ingredient1,
-            ingredient12,..etc and Recipe AI will generate an all-new recipe on
-            demand...
+            Please describe your scenario
           </p>
         </h1>
       </div>
